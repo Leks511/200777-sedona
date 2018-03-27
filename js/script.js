c7adddd218b157cdead35b4cd535b0c76baa201b
Leks511 <leks511@bk.ru>
2018-03-27 20:36:20 +0300

    Сделал анимацию ошибки формы

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 var openSearchFormButton = document.querySelector(".search-open");
 var searchForm = document.querySelector(".search-form");
+var searchFormSubmitButton = document.querySelector(".search-form-button");
 
-openSearchFormButton.addEventListener('click', function(){
+openSearchFormButton.addEventListener('click', function() {
   searchForm.classList.toggle("search-form-close");
+});
+
+searchFormSubmitButton.addEventListener('click', function() {
+
 });
