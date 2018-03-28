11e4ac2281426155d73eed8f703741952d018128
Leks511 <leks511@bk.ru>
2018-03-28 13:37:31 +0300

    Работа над формой продолжается

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 var openSearchFormButton = document.querySelector(".search-open");
-var searchForm = document.querySelector(".search-form");
-var searchFormSubmitButton = document.querySelector(".search-form-button");
+var searchForm = document.querySelector("form");
+var submitSearchFormButton = document.querySelector(".search-form-button");
+var dateEntry = searchForm.querySelector("[name=date-of-entry]");
+var dateDeparture = searchForm.querySelector("[name=date-of-departure]");
 
 openSearchFormButton.addEventListener('click', function() {
   searchForm.classList.toggle("search-form-close");
+  dateEntry.focus();
 });
 
-searchFormSubmitButton.addEventListener('click', function(evt) {
-  evt.preventDefault();
-  console.log('Клик отправить')
+searchForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  console.log(dateEntry.value);
+  console.log(dateDeparture.value);
+});
+
+submitSearchFormButton.addEventListener('click', function(e) {
+  console.log(searchForm);
 });
